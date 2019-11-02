46b3090cf3a0e7075b75bcdea46ff751a27f81ad
Jordane <jordane.frechet@gmail.com>
2019-11-02 01:32:24 +0100

    v1.0

---
+++ src/app/creation/creation.js
@@ -0,0 +1,86 @@
+/* eslint-disable max-len */
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-tabs */
+import $ from 'jquery';
+import {
+  uniq, find, propEq, sort,
+} from 'ramda';
+
+export let tshirtDisplay = [];
+const tshirtDisplayMale = [];
+const tshirtDisplayFemale = [];
+
+export const getTshirtArray = function (tshirts) {
+  const maleShirts = tshirts.filter((tee) => tee.gender === 'M');
+  const femaleShirts = tshirts.filter((tee) => tee.gender === 'F');
+  for (const tshirt of maleShirts) {
+    tshirt.size = [tshirt.size];
+    if (find(propEq('productId', tshirt.productId))(tshirtDisplayMale)) {
+      tshirtDisplayMale[tshirt.productId].size += ` ${tshirt.size}`;
+    } else {
+      tshirtDisplayMale.push(tshirt);
+    }
+  }
+
+  for (const tshirt of femaleShirts) {
+    tshirt.size = [tshirt.size];
+    if (find(propEq('productId', tshirt.productId))(tshirtDisplayFemale)) {
+      tshirtDisplayFemale[tshirt.productId].size += ` ${tshirt.size}`;
+    } else {
+      tshirtDisplayFemale.push(tshirt);
+    }
+  }
+  tshirtDisplay = sort(((a, b) => a.productId - b.productId), [...tshirtDisplayMale, ...tshirtDisplayFemale]);
+
+  tshirtDisplay.forEach((tee) => {
+	  tee.size = (tee.size.length > 1) ? tee.size.split(' ') : [tee.size];
+	  tee.size.push('any');
+  });
+};
+
+export const createTshirtDiv = function (arrayofShirts) {
+  for (const tee of arrayofShirts) {
+    const { gender } = tee;
+    const uniqueSizes = uniq(tee.size);
+    const sizesDisplay = uniqueSizes.map((e) => ((e !== 'any') ? `<span>${e}</span>` : '')).join('');
+
+    const genderIconUrl = (gender === 'F') ? 'public/assets/images/female.png' : 'public/assets/images/male.png';
+    const divTee = $(`
+		<div id="tShirt">
+			<img src="${tee.imageUrl}" alt="${tee.category} t-shirt"/>
+			<div id="tshirtText">
+				<div>
+					<p>${tee.price}<sup>€</sup></p>
+					<img src="${genderIconUrl}" alt="" id="genderIcon"/>
+				</div>
+				<div>
+					${sizesDisplay}
+				</div>
+			</div>
+		</div>`);
+    divTee.appendTo($('section'));
+  }
+};
+
+export const createSizeButtons = function (arrayOfShirts) {
+  const uniqueSizes = uniq(arrayOfShirts.map((tee) => tee.size)
+	  .join(',')
+	  .split(','))
+	  .map((size) => ((size !== 'any') ? `<button class="btn-filter">${size}</button>` : ''))
+	  .join('');
+  const buttonsSize = $(`${uniqueSizes}`);
+  buttonsSize.appendTo($('#sizeFilter'));
+};
+
+export const createCatButtons = function (arrayOfShirts) {
+  const categories = uniq(arrayOfShirts.map((shirt) => shirt.category))
+	  .map((category) => `<button class="btn-filter">${category}</button>`)
+	  .join('');
+  const buttonsCat = $(`${categories}`);
+  buttonsCat.appendTo($('#catFilter'));
+};
+
+export const filteredTshirtDisplay = (arrayOfShirts, currGender, currSize, currCategory, currPrice) => arrayOfShirts.filter((shirt) => (shirt.gender === currGender || currGender === '')
+        && shirt.size.includes(currSize)
+        && (shirt.category === currCategory || currCategory === '')
+		&& shirt.price <= currPrice);
--- src/index.js
+++ src/index.js
@@ -1,76 +1,98 @@
+/* eslint-disable max-len */
 /* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable no-tabs */
 import $ from 'jquery';
-import { get } from 'axios';
+import { get, post } from 'axios';
 import {
-  find, uniq, prop, propEq,
-  map as Rmap, sort,
+  max,
 } from 'ramda';
+import validator from 'validator';
+import {
+  createTshirtDiv, getTshirtArray, tshirtDisplay, createSizeButtons, createCatButtons, filteredTshirtDisplay,
+} from './app/creation/creation';
 
-let tshirtDisplay = [];
-const tshirtDisplayMale = [];
-const tshirtDisplayFemale = [];
+let currentGender = '';
+let currentSize = '';
+let currentCat = '';
+let highestPrice = '';
+const setValues = () => {
+  currentSize = currentSize || 'any';
+  currentGender = currentGender || '';
+  highestPrice = highestPrice || tshirtDisplay.map((shirt) => shirt.price).reduce((a, b) => max(a, b), 0);
+  currentCat = currentCat || '';
+};
 
 get('http://localhost:3000/t-shirts')
   .then((response) => response.data)
   .then((tshirts) => {
-    const maleShirts = tshirts.filter((tee) => tee.gender === 'M');
-    const femaleShirts = tshirts.filter((tee) => tee.gender === 'F');
-
-    for (const tshirt of maleShirts) {
-      tshirt.size = [tshirt.size];
-      if (find(propEq('productId', tshirt.productId))(tshirtDisplayMale)) {
-        tshirtDisplayMale[tshirt.productId].size += ` ${tshirt.size}`;
-      } else {
-        tshirtDisplayMale.push(tshirt);
-      }
-    }
-
-    for (const tshirt of femaleShirts) {
-      tshirt.size = [tshirt.size];
-      if (find(propEq('productId', tshirt.productId))(tshirtDisplayFemale)) {
-        tshirtDisplayFemale[tshirt.productId].size += ` ${tshirt.size}`;
-      } else {
-        tshirtDisplayFemale.push(tshirt);
-      }
-    }
-
-    tshirtDisplay = sort(((a, b) => a.productId - b.productId), [...tshirtDisplayMale, ...tshirtDisplayFemale]);
-    // you stopped here, now to display the correct things, sorted should be finished
-
-    console.log(tshirtDisplay);
-    tshirtDisplay.forEach((tee) => {
-	  tee.size = tee.size.split(' ');
-	   // ?
-    });
+    getTshirtArray(tshirts);
   })
   .then(() => {
-    for (const tee of tshirtDisplay) {
-	  const { gender } = tee;
-	  const uniqueSizes = uniq(tee.size); // ?
-	  const sizesDisplay = uniqueSizes.map((e) => `<span>${e}</span>`).join('');
-      const genderIconUrl = (gender === 'F') ? 'public/assets/images/female.png' : 'public/assets/images/male.png';
-      const divTee = $(`
-		  <div id="tShirt">
-			  <img
-				  src="${tee.imageUrl}"
-				  alt=""
-			  />
-			  <div id="tshirtText">
-				  <div>
-					  <p>${tee.price}<sup>€</sup></p>
-					  <img
-						  src="${genderIconUrl}"
-						  alt=""
-						  id="genderIcon"
-					  />
-				  </div>
-				  <div>
-					  ${sizesDisplay}
-				  </div>
-			  </div>
-		  </div>`);
-      divTee.appendTo($('section'));
-	  }
+    createSizeButtons(tshirtDisplay);
+    createCatButtons(tshirtDisplay);
+    createTshirtDiv(tshirtDisplay);
+  })
+  .then(() => {
+    $('#genderFilter > button').on('click', (e) => {
+      setValues();
+      currentGender = e.target.innerHTML;
+      const newTshirtDisplay = filteredTshirtDisplay(tshirtDisplay, currentGender, currentSize, currentCat, highestPrice);
+      $('section').empty();
+      createTshirtDiv(newTshirtDisplay);
+    });
+
+    $('#sizeFilter > button').on('click', (e) => {
+      setValues();
+      currentSize = e.target.innerHTML;
+      const newTshirtDisplay = filteredTshirtDisplay(tshirtDisplay, currentGender, currentSize, currentCat, highestPrice);
+      $('section').empty();
+      createTshirtDiv(newTshirtDisplay);
+    });
+
+    $('#catFilter > button').on('click', (e) => {
+      setValues();
+      currentCat = e.target.innerHTML;
+      const newTshirtDisplay = filteredTshirtDisplay(tshirtDisplay, currentGender, currentSize, currentCat, highestPrice);
+      $('section').empty();
+      createTshirtDiv(newTshirtDisplay);
+    });
+
+    $('#maxPriceInput').on('input', (e) => {
+      setValues();
+      highestPrice = e.target.valueAsNumber;
+      const newTshirtDisplay = filteredTshirtDisplay(tshirtDisplay, currentGender, currentSize, currentCat, highestPrice);
+      if (isNaN(highestPrice)) {
+        highestPrice = tshirtDisplay.map((shirt) => shirt.price).reduce((a, b) => max(a, b), 0);
+      }
+      $('section').empty();
+      createTshirtDiv(newTshirtDisplay);
+    });
+
+    $('#allShirts > button').on('click', () => {
+      $('section').empty();
+      createTshirtDiv(tshirtDisplay);
+    });
   });
-// ?
+
+
+$('#uploadSubmit').on('click', (e) => {
+  if (validator.isIn($('#sizeUpload').val(), ['XXS', 'XS', 'S', 'M', 'L', 'XL', 'XXL'])
+      && validator.isNumeric($('#priceUpload').val())
+      && validator.isIn($('#genderUpload').val(), ['M', 'F'])
+      && validator.isURL($('#shirtImgUpload').val(), { allow_underscores: true })) {
+    post('http://localhost:3000/t-shirts', {
+      id: tshirtDisplay.map((shirt) => shirt.productId).reduce((a, b) => max(a, b), 0) + 1,
+      productId: tshirtDisplay.map((shirt) => shirt.productId).reduce((a, b) => max(a, b), 0) + 1,
+      price: $('#priceUpload').val(),
+      size: $('#sizeUpload').val(),
+      gender: $('#genderUpload').val(),
+      imageUrl: $('#shirtImgUpload').val(),
+      category: $('#catUpload').val(),
+    })
+      .then(() => {
+        location.reload();
+      });
+  } else {
+    alert('Please enter a correct URL, a size between XXS and XXL, a correct gender (F or M) and a correct price');
+  }
+});
